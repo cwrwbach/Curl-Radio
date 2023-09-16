75ae90162fcc40713c6b3d1780fabf0c3db1dcec
cwrwbach <cwrwbach@daijames.plus.com>
2023-09-16 14:10:42 +0100

    Coming on but header is still confusing me?

--- curl-radio.c
+++ curl-radio.c
@@ -19,27 +19,63 @@
 //"http://icecast.thisisdax.com/SmoothUKMP3"
  
 FILE *bodyfile;
-//FILE * headerfile;
+FILE *headerfile;
+
+unsigned int debug;
+
 
 static size_t data_callback(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-char * test =  strstr(ptr,"tream");
+char * test =  strstr(ptr,"Stream");
+char  * t2 = ptr;
+unsigned char show;
 if(test > 0)
     {
-    printf(" HIT %d \n",test);
+    printf(" HIT %d: %d \n",debug,test);
     printf(" %s \n",test);
     }
 
+show = t2[debug++];
+
+//if(debug < 256)
+ //   printf("%2.2d: %2.2x %c \n",debug,show,show);
+
+//if(debug %40 == 0) printf("\n");
 size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
 return written;
 }
 
-static size_t write_header(void *ptr, size_t size, size_t nmemb, void *stream)
+static size_t xxxheader_callback(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+char * test =  strstr(ptr,"Stream");
+
+test = stream;
+
+//if(test > 0)
+    {
+    printf(" HEAD %d: %d \n",debug,test);
+    printf(" %s \n",test);
+    }
+return 10; //written;
+}
+
+
+static size_t header_callback(void *ptr, size_t size, size_t nmemb, void *stream)
 {
 size_t written = fwrite(ptr, size, nmemb, stream);
 printf(" writ %d %s \n",written,ptr);
 return written;
 }
+
+
+//size_t written = fwrite(ptr, size, nmemb, (FILE * )stream);
+//written = fwrite(headerfile,1,100,stream);
+
+
+
+//fwrite(headerfile,1,100,(FILE *) stream);
+
+
 
 //---
  
@@ -48,6 +84,9 @@ int main(void)
 CURL *curl_handle;
 char * curl_ver;
 static const char *bodyfilename = "body-ok.mp3";
+static const char *headerfilename = "header-ok.mp3";
+
+debug = 0;
 curl_ver = curl_version(); 
 printf(" CURL based ICECAST client.\nVESION: %s \n\n",curl_ver);
 
@@ -67,6 +106,8 @@ curl_easy_setopt(curl_handle, CURLOPT_URL,"http://icecast.thisisdax.com/SmoothUK
 //send all data to this function 
 curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, data_callback);
 
+//curl_easy_setopt(curl_handle, CURLOPT_HEADERFUNCTION, header_callback);
+
 //open the body file 
 bodyfile = fopen(bodyfilename, "wb");
 if(!bodyfile) 
@@ -76,6 +117,17 @@ if(!bodyfile)
     return -1;
     }
 
+
+//open the header file 
+headerfile = fopen(headerfilename, "wb");
+if(!bodyfile) 
+    {
+    curl_easy_cleanup(curl_handle);
+    fclose(headerfile);
+    return -1;
+    }
+
+
 headers = curl_slist_append(headers, "Icy-MetaData:1"); //Definitely needed
 curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, headers); //Needed
 
@@ -84,8 +136,14 @@ curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, headers); //Needed
 
 //curl_easy_setopt(curl_handle,CURLOPT_HEADER,1);
 
+//curl_easy_setopt(curl_handle,CURLOPT_VERBOSE,1);
+
 //we want the body be written to this file handle instead of stdout */
 curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, bodyfile);
+
+//we want the headers be written to this file handle instead of stdout */
+//curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, headerfile);
+
 
 //Execute Curl - and loop for ever
 curl_easy_perform(curl_handle); //does not return
